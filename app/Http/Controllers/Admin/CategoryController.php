94efdb073fc66a694ae67627e68637189ce68250
PronayBormon <pronaykumarbormon@gmail.com>
2023-05-25 12:47:59 +0600

    read option complate

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -12,7 +12,8 @@ class CategoryController extends Controller
 {
     public function index()
     {
-        return view('admin.category.index');
+        $category = category::all();
+        return view('admin.category.index', compact('category'));
     }
     public function create()
     {
--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -15,13 +15,32 @@
                 <tr>
                     <th>Id</th>
                     <th>name</th>
+                    <th>slug</th>
+                    <th>Description</th>
+                    <th>meta Title </th>
+                    <th>status</th>
+                    <th>Created By</th>
+                    <th>Image</th>
+                    <th>Edit/Delete</th>
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td>111</td>
-                    <td>Pronay</td>
-                </tr>
+                @foreach($category as $item)
+                    <tr>
+                        <td>{{$item->id}}</td>
+                        <td>{{$item->name}}</td>
+                        <td>{{$item->slug}}</td>
+                        <td>{{$item->description}}</td>
+                        <td>{{$item->meta_title}}</td>
+                        <td>{{$item->status== true?'Active':'Inactive'}}</td>
+                        <td>{{$item->created_by}}</td>
+                        <td class="text-center"><img src="{{ asset('public/images/category/'.$item->image)}}" width="40px" alt=""></td>
+                        <td class="btn-group btn-sm"> 
+                            <a href="" class="btn btn-warning">Edit</a>
+                            <a href="" class="btn btn-danger">Delete</a>
+                        </td>
+                    </tr>
+                @endforeach
             </tbody>
 
         </table>
